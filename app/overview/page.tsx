bc64f5d2eea343e178ab2c217b86d51406db4c9f
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-14 20:50:09 +0100

    link home and overview page with buttons
    Overview and home page can no go back and forth between one another by <Link>

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const CompaniesOverview = () => {
   return (
-    <main>
-
+    <main className='grid place-items-center h-56'>
+        <h1 className='text-sky-900'>
+            Congratulations!!! you made it to a new page
+        </h1>
+        <Link href={"\."} className='btn btn-outline'>Go Back</Link>
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center p-76'>
       <h1 className='text-sky-400 py-10'>Start Page</h1>
+      <Link className='btn btn-outline' href="overview" > Press me </Link>
     </main>
   )
 }
