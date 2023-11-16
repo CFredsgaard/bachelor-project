2e761e6bc5a8003cfd242f38478aaf638b0f21ce
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-17 00:40:58 +0100

    Components created

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,14 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
+import companiesJSON from "../data/companies.json";
+import CompanyList from "./displayCompanies/CompanyList";
 
 const CompaniesOverview = () => {
-  return (
-    <>
-    <main>
-      
-    </main>
-    </>
-  )
-}
+    const companies = companiesJSON;
 
-export default CompaniesOverview
+    //console.log(companies);
+
+    return (
+        <>
+            <main className="p-4">
+                <CompanyList companies={companies} />
+            </main>
+        </>
+    );
+};
+
+export default CompaniesOverview;
