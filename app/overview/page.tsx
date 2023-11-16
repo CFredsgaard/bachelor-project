fc47ad2d37a2f0ecbb5bcb86a8d01642b1a5b60b
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-16 23:31:28 +0100

    Clean up the overview page

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 
 const CompaniesOverview = () => {
   return (
-    <main className='grid place-items-center h-56'>
-        <h1 className='text-sky-900'>
-            Congratulations!!! you made it to a new page
-        </h1>
-        <Link href={"\."} className='btn btn-outline'>Go Back</Link>
+    <>
+    <main>
+      
     </main>
+    </>
   )
 }
 
