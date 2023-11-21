8e316dcf200162165cb2c4d89ef77ee679ee7196
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-21 23:40:15 +0100

    Add content to Header component (not functional)
    The Header does not have functionality yet, but is visually ok

--- app/overview/header/Header.tsx
+++ app/overview/header/Header.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link";
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className="navbar bg-base-100 grid grid-cols-5">
+            <div className="col-span-2">
+                <Link href="/" className="text-xl ">
+                    Work Life Balance
+                </Link>
+            </div>
+            <div className="self-center col-span-1">
+                <input
+                    type="text"
+                    placeholder="Not yet implemented"
+                    className="input input-bordered input-primary input-md w-full max-w-xs"
+                />
+                <button className="btn btn-outline btn-primary btn-md m-2">Search</button>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
