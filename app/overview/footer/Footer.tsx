406db64c6871ed2e66ddb561a545073b99962aa0
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-21 23:14:58 +0100

    Add content to Footer component

--- app/overview/footer/Footer.tsx
+++ app/overview/footer/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <aside>
+                <p>Bachelor Project - 2023</p>
+            </aside>
+        </footer>
+    );
 };
 
 export default Footer;
