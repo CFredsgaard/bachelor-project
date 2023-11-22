7ff895e5595574033ed8b75bed3d2223bfff133b
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-22 16:58:22 +0100

    Change RadioButton input style

--- app/overview/sidebar/sorting/RadioButton.tsx
+++ app/overview/sidebar/sorting/RadioButton.tsx
@@ -3,7 +3,13 @@ import { ChangeEventHandler } from "react";
 const RadioButton = (props: { label: string; value: string; name: string; onChange: ChangeEventHandler }) => {
     return (
         <label className="label cursor-pointer justify-start space-x-2">
-            <input className="radio radio-primary" type="radio" value={props.value} name={props.name} onChange={props.onChange} />
+            <input
+                className="radio border-neutral checked:bg-secondary checked:border-none"
+                type="radio"
+                value={props.value}
+                name={props.name}
+                onChange={props.onChange}
+            />
             <span className="label-text">{props.label}</span>
         </label>
     );
