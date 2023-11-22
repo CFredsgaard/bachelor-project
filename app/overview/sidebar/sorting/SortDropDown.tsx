afeadcfe285e9874b772d2cb511492ed9352cdf3
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-22 14:28:07 +0100

    Add a RadioButton to SortDropDown (placeholder, no content)

--- app/overview/sidebar/sorting/SortDropDown.tsx
+++ app/overview/sidebar/sorting/SortDropDown.tsx
@@ -1,4 +1,5 @@
 import CompanyDecorator from "@/app/models/CompanyDecorator";
+import RadioButton from "./RadioButton";
 
 const SortDropDown = (props: { companies: CompanyDecorator[]; updateCompanies: (selectedCompanies: CompanyDecorator[]) => void }) => {
     return (
@@ -6,7 +7,9 @@ const SortDropDown = (props: { companies: CompanyDecorator[]; updateCompanies: (
             <details className="collapse collapse-arrow bg-secondary">
                 <summary className="collapse-title text-m font-medium">Sort by</summary>
                 <div className="collapse-content">
-                    <div className="form-control"></div>
+                    <div className="form-control">
+                        <RadioButton />
+                    </div>
                 </div>
             </details>
         </div>
