472df432856a797773588dd2051a4f75f283f892
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-22 14:29:03 +0100

    Add dummy content to RadioButton

--- app/overview/sidebar/sorting/RadioButton.tsx
+++ app/overview/sidebar/sorting/RadioButton.tsx
@@ -1,5 +1,10 @@
 const RadioButton = () => {
-    return <div>RadioButton</div>;
+    return (
+        <label className="label cursor-pointer justify-start space-x-2">
+            <input className="radio radio-primary" type="radio" name="Name Here" value="Value Here" onChange={() => {}} />
+            <span className="label-text">Label here</span>
+        </label>
+    );
 };
 
 export default RadioButton;
