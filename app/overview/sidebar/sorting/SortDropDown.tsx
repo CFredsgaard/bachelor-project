1b16d3f62370bdaf4f4c57d89bf7cb36813c2c76
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-22 14:23:39 +0100

    Add dropdown element inside return
    No radiobuttons inside yet and no functionality

--- app/overview/sidebar/sorting/SortDropDown.tsx
+++ app/overview/sidebar/sorting/SortDropDown.tsx
@@ -1,7 +1,16 @@
 import CompanyDecorator from "@/app/models/CompanyDecorator";
 
 const SortDropDown = (props: { companies: CompanyDecorator[]; updateCompanies: (selectedCompanies: CompanyDecorator[]) => void }) => {
-    return <div>SortDropDown</div>;
+    return (
+        <div>
+            <details className="collapse collapse-arrow bg-secondary">
+                <summary className="collapse-title text-m font-medium">Sort by</summary>
+                <div className="collapse-content">
+                    <div className="form-control"></div>
+                </div>
+            </details>
+        </div>
+    );
 };
 
 export default SortDropDown;
