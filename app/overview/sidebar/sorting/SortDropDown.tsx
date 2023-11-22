e9da72b9db7097517eae018477b084e162176bd1
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-22 15:27:12 +0100

    Add enum for sort options and use it instead of hardcoded strings

--- app/overview/sidebar/sorting/SortDropDown.tsx
+++ app/overview/sidebar/sorting/SortDropDown.tsx
@@ -6,6 +6,29 @@ import { useState } from "react";
 const SortDropDown = (props: { companies: CompanyDecorator[]; updateCompanies: (selectedCompanies: CompanyDecorator[]) => void }) => {
     const [sortBy, setSortBy] = useState<string>("");
 
+    enum SortOptions {
+        ALPHABETICALLY = "alphabetically",
+        LOCATION = "location",
+        MOST_WOMEN_TECH_ROLES = "mostWomenTechRoles",
+        MOST_WOMEN_LEADERSHIP = "mostWomenLeadership",
+        MOST_MEN_TECH_ROLES = "mostMenTechRoles",
+        MOST_MEN_LEADERSHIP = "mostMenLeadership",
+    }
+
+    // Type to map sort functions to option
+    type SortFunctions = {
+        [key: string]: (a: CompanyDecorator, b: CompanyDecorator) => number;
+    };
+
+    const sortFunctions: SortFunctions = {
+        [SortOptions.ALPHABETICALLY]: (a, b) => a.company.name.localeCompare(b.company.name),
+        [SortOptions.LOCATION]: (a, b) => a.company.location.localeCompare(b.company.location),
+        [SortOptions.MOST_WOMEN_TECH_ROLES]: (a, b) => b.percentageWomenInTechRoles - a.percentageWomenInTechRoles,
+        [SortOptions.MOST_WOMEN_LEADERSHIP]: (a, b) => b.percentageWomenInLeadership - a.percentageWomenInLeadership,
+        [SortOptions.MOST_MEN_TECH_ROLES]: (a, b) => b.percentageMenInTechRoles - a.percentageMenInTechRoles,
+        [SortOptions.MOST_MEN_LEADERSHIP]: (a, b) => b.percentageMenInLeadership - a.percentageMenInLeadership,
+    };
+
     // Type to map the radio button sorting options label and value
     type RadioButtonSortingOptions = {
         label: string;
@@ -13,12 +36,12 @@ const SortDropDown = (props: { companies: CompanyDecorator[]; updateCompanies: (
     };
 
     const radioButtonSortingOptions: RadioButtonSortingOptions[] = [
-        { label: "Alphabetically", value: "alphabetically" },
-        { label: "Location", value: "location" },
-        { label: "Most women in tech roles", value: "mostWomenTechRoles" },
-        { label: "Most women in leadership", value: "mostWomenLeadership" },
-        { label: "Most men in tech roles", value: "mostMenTechRoles" },
-        { label: "Most men in leadership", value: "mostMenLeadership" },
+        { label: "Alphabetically", value: SortOptions.ALPHABETICALLY },
+        { label: "Location", value: SortOptions.LOCATION },
+        { label: "Most women in tech roles", value: SortOptions.MOST_WOMEN_TECH_ROLES },
+        { label: "Most women in leadership", value: SortOptions.MOST_WOMEN_LEADERSHIP },
+        { label: "Most men in tech roles", value: SortOptions.MOST_MEN_TECH_ROLES },
+        { label: "Most men in leadership", value: SortOptions.MOST_MEN_LEADERSHIP },
     ];
 
     // Sets the sortBy option when onChange is triggered
