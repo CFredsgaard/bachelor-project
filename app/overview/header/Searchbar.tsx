87bbccaf5a6829790a07dc9622c7298628be42f6
Kaan <kaan1705@gmail.com>
2023-11-24 20:03:17 +0100

    Added search functionality (Not working properly yet)

---
+++ app/overview/header/Searchbar.tsx
@@ -0,0 +1,44 @@
+import CompanyDecorator from "@/app/models/CompanyDecorator";
+import React, { useState } from "react";
+
+interface CompanySearchBarProps {
+  companies: CompanyDecorator[];
+  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+}
+
+const Searchbar: React.FC<CompanySearchBarProps> = ({
+  companies,
+  updateCompanies,
+}) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const handleCompanySearch = () => {
+    //Retrieve all companies
+    const CompanyList = [...companies];
+    console.log(searchTerm);
+    //Filter based on search terms
+    const filteredData = CompanyList.filter((item) =>
+      item.company.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    //Display companies matching search terms
+    updateCompanies(filteredData);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for a company..."
+        className="input input-bordered input-primary input-md w-full max-w-xs"
+        onChange={(evt) => setSearchTerm(evt.target.value)}
+      />
+      <button
+        onClick={handleCompanySearch}
+        className="btn btn-outline btn-primary btn-md m-2"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default Searchbar;
--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -10,33 +10,42 @@ import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 
-let _companies = companiesJSON.map((company) => new CompanyDecorator(company as CompanyData));
+const _companies = companiesJSON.map((company) => new CompanyDecorator(company as CompanyData));
+
+const handleSearch = (type: string, searchTerm: string) => {
+  console.log(`Searching for ${type}: ${searchTerm}`);
+};
 
 const CompaniesOverview = () => {
     const [displayedCompanies, setDisplayedCompanies] = useState<CompanyDecorator[]>(_companies);
     const updateDisplayedCompanies = (selectedCompanies: CompanyDecorator[]) => setDisplayedCompanies(selectedCompanies);
 
     return (
-        <>
-            <Header />
+      <>
+        <Header
+        companies = {displayedCompanies} 
+        updateCompanies={updateDisplayedCompanies}
+        />
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
 
-            <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+          {/** Main content of page */}
+          <div className="drawer-content flex flex-col items-center justify-center">
+            <main className="p-4">
+              <CompanyList companies={displayedCompanies} />
+            </main>
+          </div>
 
-                {/** Main content of page */}
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    <main className="p-4">
-                        <CompanyList companies={displayedCompanies} />
-                    </main>
-                </div>
-
-                {/* Sidebar */}
-                <div className="drawer-side">
-                    <Sidebar companies={displayedCompanies} updateCompanies={updateDisplayedCompanies} />
-                </div>
-            </div>
-            <Footer />
-        </>
+          {/* Sidebar */}
+          <div className="drawer-side">
+            <Sidebar
+              companies={displayedCompanies}
+              updateCompanies={updateDisplayedCompanies}
+            />
+          </div>
+        </div>
+        <Footer />
+      </>
     );
 };
 
