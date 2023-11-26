1ad395418330dd72d685d45141587763bcdfd466
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-26 15:18:31 +0100

    Make CheckBox from list of citites

--- app/overview/sidebar/filtering/LocationGroup.tsx
+++ app/overview/sidebar/filtering/LocationGroup.tsx
@@ -1,31 +1,16 @@
 import CompanyDecorator from "@/app/models/CompanyDecorator";
 import React from "react";
+import CheckBox from "./CheckBox";
 
 const LocationGroup = (props: { companies: CompanyDecorator[]; updateCompanies: (selectedCompanies: CompanyDecorator[]) => void }) => {
+    const cities = ["Aarhus", "Odense", "Copenhagen"];
     return (
         <div>
-            <div className="form-control pt-4">
-                <h1 className="md:font-bold text-lg">Location</h1>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Aalborg</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Aarhus</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Copenhagen</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Herning</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Odense</span>
-                </label>
+            <h1 className="md:font-bold text-lg">Location</h1>
+            <div className="form-control overflow-scroll h-96 bg-base-100">
+                {cities.map((city) => (
+                    <CheckBox label={city} value={city} onChange={() => {}} />
+                ))}
             </div>
         </div>
     );
