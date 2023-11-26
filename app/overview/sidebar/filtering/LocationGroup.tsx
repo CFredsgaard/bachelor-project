cc6b0684462e508462d7583833c011c73e8d065e
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-26 15:06:24 +0100

    Add dummy data to LocationGroup

--- app/overview/sidebar/filtering/LocationGroup.tsx
+++ app/overview/sidebar/filtering/LocationGroup.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const LocationGroup = () => {
-    return <div>LocationGroup</div>;
+    return (
+        <div>
+            <div className="form-control pt-4">
+                <h1 className="md:font-bold text-lg">Location</h1>
+                <label className="label cursor-pointer justify-start space-x-2">
+                    <input type="checkbox" className="checkbox" />
+                    <span className="label-text">Aalborg</span>
+                </label>
+                <label className="label cursor-pointer justify-start space-x-2">
+                    <input type="checkbox" className="checkbox" />
+                    <span className="label-text">Aarhus</span>
+                </label>
+                <label className="label cursor-pointer justify-start space-x-2">
+                    <input type="checkbox" className="checkbox" />
+                    <span className="label-text">Copenhagen</span>
+                </label>
+                <label className="label cursor-pointer justify-start space-x-2">
+                    <input type="checkbox" className="checkbox" />
+                    <span className="label-text">Herning</span>
+                </label>
+                <label className="label cursor-pointer justify-start space-x-2">
+                    <input type="checkbox" className="checkbox" />
+                    <span className="label-text">Odense</span>
+                </label>
+            </div>
+        </div>
+    );
 };
 
 export default LocationGroup;
