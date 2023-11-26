9d1fd5fde4bc8c32588f4718fbfcd601216791b4
Kaan <kaan1705@gmail.com>
2023-11-26 17:10:37 +0100

    Added location search

--- app/overview/header/Header.tsx
+++ app/overview/header/Header.tsx
@@ -3,26 +3,28 @@ import Searchbar from "./Searchbar";
 import CompanyDecorator from "@/app/models/CompanyDecorator";
 
 const Header = (props: {
-    allCompanies: CompanyDecorator[];
-    updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
-    updateShouldSort: (shouldSort: boolean) => void;
+  allCompanies: CompanyDecorator[];
+  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+  updateShouldSort: (shouldSort: boolean) => void;
 }) => {
-    return (
-        <div className="navbar bg-base-100 grid grid-cols-5">
-            <div className="col-span-2 h-full">
-                <Link href="/" className="h-full">
-                    <p className="text-3xl font-bold h-full flex flex-wrap p-4 place-content-evenly">Work Life Balance</p>
-                </Link>
-            </div>
-            <div className="self-center col-span-1">
-                <Searchbar
-                    updateCompanies={props.updateCompanies}
-                    allCompanies={props.allCompanies}
-                    updateShouldSort={props.updateShouldSort}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="navbar bg-base-100 grid grid-cols-5">
+      <div className="col-span-2 h-full">
+        <Link href="/" className="h-full">
+          <p className="text-3xl font-bold h-full flex flex-wrap p-4 place-content-evenly">
+            Work Life Balance
+          </p>
+        </Link>
+      </div>
+      <div className="self-center col-span-1">
+        <Searchbar
+          updateCompanies={props.updateCompanies}
+          allCompanies={props.allCompanies}
+          updateShouldSort={props.updateShouldSort}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -10,43 +10,51 @@ import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 
-const allCompanies = companiesJSON.map((company) => new CompanyDecorator(company as CompanyData));
+const allCompanies = companiesJSON.map(
+  (company) => new CompanyDecorator(company as CompanyData)
+);
 
 const CompaniesOverview = () => {
-    const [displayedCompanies, setDisplayedCompanies] = useState<CompanyDecorator[]>(allCompanies);
-    const [shouldSort, setShouldSort] = useState<boolean>(true);
+  const [displayedCompanies, setDisplayedCompanies] =
+    useState<CompanyDecorator[]>(allCompanies);
+  const [shouldSort, setShouldSort] = useState<boolean>(true);
 
-    const updateDisplayedCompanies = (selectedCompanies: CompanyDecorator[]) => setDisplayedCompanies(selectedCompanies);
-    const updateShouldSort = (shouldSort: boolean) => {
-        setShouldSort(shouldSort);
-    };
+  const updateDisplayedCompanies = (selectedCompanies: CompanyDecorator[]) =>
+    setDisplayedCompanies(selectedCompanies);
+  const updateShouldSort = (shouldSort: boolean) => {
+    setShouldSort(shouldSort);
+  };
 
-    return (
-        <div className="flex flex-col">
-            <div className="fixed top-0 w-full z-20">
-                <Header updateCompanies={updateDisplayedCompanies} allCompanies={allCompanies} updateShouldSort={updateShouldSort} />
-            </div>
+  return (
+    <div className="flex flex-col">
+      <div className="fixed top-0 w-full z-20">
+        <Header
+          updateCompanies={updateDisplayedCompanies}
+          allCompanies={allCompanies}
+          updateShouldSort={updateShouldSort}
+        />
+      </div>
 
-            <div className="grid grid-cols-6 mt-20 mb-20 pt-4">
-                <div className="fixed top-0 pt-20 h-full col-span-1">
-                    <Sidebar
-                        companies={displayedCompanies}
-                        updateCompanies={updateDisplayedCompanies}
-                        updateShouldSort={updateShouldSort}
-                        shouldSort={shouldSort}
-                    />
-                </div>
-
-                <main className="col-start-2 col-span-5">
-                    <CompanyList companies={displayedCompanies} />
-                </main>
-            </div>
-
-            <div className="bottom-0 w-full z-50">
-                <Footer />
-            </div>
+      <div className="grid grid-cols-6 mt-20 mb-20 pt-4">
+        <div className="fixed top-0 pt-20 h-full col-span-1">
+          <Sidebar
+            companies={displayedCompanies}
+            updateCompanies={updateDisplayedCompanies}
+            updateShouldSort={updateShouldSort}
+            shouldSort={shouldSort}
+          />
         </div>
-    );
+
+        <main className="col-start-2 col-span-5">
+          <CompanyList companies={displayedCompanies} />
+        </main>
+      </div>
+
+      <div className="bottom-0 w-full z-50">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default CompaniesOverview;
