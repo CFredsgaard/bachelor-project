703dc21dbc1e9120a4383b75f540f77f8ccc3412
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-26 13:54:41 +0100

    Added state to track if sorting should be done after search

--- app/overview/header/Header.tsx
+++ app/overview/header/Header.tsx
@@ -2,33 +2,27 @@ import Link from "next/link";
 import Searchbar from "./Searchbar";
 import CompanyDecorator from "@/app/models/CompanyDecorator";
 
-interface CompanySearchBarProps {
-  companies: CompanyDecorator[];
-  allCompanies: CompanyDecorator[];
-  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
-}
-
-const Header: React.FC<CompanySearchBarProps> = ({
-  companies,
-  allCompanies,
-  updateCompanies,
+const Header = (props: {
+    allCompanies: CompanyDecorator[];
+    updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+    updateShouldSort: (shouldSort: boolean) => void;
 }) => {
-  return (
-    <div className="navbar bg-base-100 grid grid-cols-5">
-      <div className="col-span-2">
-        <Link href="/" className="text-xl ">
-          Work Life Balance
-        </Link>
-      </div>
-      <div className="self-center col-span-1">
-        <Searchbar
-          updateCompanies={updateCompanies}
-          companies={companies}
-          allCompanies={allCompanies}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar bg-base-100 grid grid-cols-5">
+            <div className="col-span-2 h-full">
+                <Link href="/" className="h-full">
+                    <p className="text-3xl font-bold h-full flex flex-wrap p-4 place-content-evenly">Work Life Balance</p>
+                </Link>
+            </div>
+            <div className="self-center col-span-1">
+                <Searchbar
+                    updateCompanies={props.updateCompanies}
+                    allCompanies={props.allCompanies}
+                    updateShouldSort={props.updateShouldSort}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Header;
--- app/overview/header/Searchbar.tsx
+++ app/overview/header/Searchbar.tsx
@@ -2,45 +2,38 @@ import CompanyDecorator from "@/app/models/CompanyDecorator";
 import React, { useState } from "react";
 
 interface CompanySearchBarProps {
-  companies: CompanyDecorator[];
-  allCompanies: CompanyDecorator[];
-  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+    allCompanies: CompanyDecorator[];
+    updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+    updateShouldSort: (shouldSort: boolean) => void;
 }
 
-const Searchbar: React.FC<CompanySearchBarProps> = ({
-  companies,
-  allCompanies,
-  updateCompanies,
-}) => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const handleCompanySearch = () => {
-    //Retrieve all companies
-    const companyList = [...allCompanies];
-    console.log(searchTerm);
-    //Filter based on search terms
-    const filteredData = companyList.filter((item) =>
-      item.company.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    //Display companies matching search terms
-    updateCompanies(filteredData);
-  };
+const Searchbar: React.FC<CompanySearchBarProps> = ({ allCompanies, updateCompanies, updateShouldSort }) => {
+    const [searchTerm, setSearchTerm] = useState<string>("");
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for a company..."
-        className="input input-bordered input-primary input-md w-full max-w-xs"
-        onChange={(evt) => setSearchTerm(evt.target.value)}
-      />
-      <button
-        onClick={handleCompanySearch}
-        className="btn btn-outline btn-primary btn-md m-2"
-      >
-        Search
-      </button>
-    </div>
-  );
+    const handleCompanySearch = () => {
+        //Retrieve all companies
+        const companyList = [...allCompanies];
+        console.log(searchTerm);
+        //Filter based on search terms
+        const filteredData = companyList.filter((item) => item.company.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        //Display companies matching search terms
+        updateCompanies(filteredData);
+        updateShouldSort(true);
+    };
+
+    return (
+        <div className="">
+            <input
+                type="text"
+                placeholder="Search for a company..."
+                className="input input-bordered input-primary input-md w-full max-w-xs"
+                onChange={(evt) => setSearchTerm(evt.target.value)}
+            />
+            <button className="btn btn-outline btn-primary btn-md m-2" onClick={handleCompanySearch}>
+                Search
+            </button>
+        </div>
+    );
 };
 
 export default Searchbar;
