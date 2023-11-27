af86eb76986d094cb08ae12be5d0a272b0ce7f6f
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-27 22:04:34 +0100

    Better feedback message when no data matches selected criteria

--- src/app/overview/displayCompanies/CompanyList.tsx
+++ src/app/overview/displayCompanies/CompanyList.tsx
@@ -5,7 +5,19 @@ const CompanyList = (props: { companies: CompanyDecorator[] }) => {
     const companies: CompanyDecorator[] = props.companies;
 
     if (!companies || companies.length === 0) {
-        return <div>No data availabe</div>;
+        return (
+            <div className="flex justify-center items-center mt-96">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6 mr-2">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                </svg>
+                <span>No data matching your criteria</span>
+            </div>
+        );
     }
 
     return (
