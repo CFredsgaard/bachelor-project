1893f19a33ff35f52815f6d5cd56f2c3fce90443
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-28 11:01:40 +0100

    Move dummy data from SideBar to LocationGroup

--- src/app/overview/sidebar/Sidebar.tsx
+++ src/app/overview/sidebar/Sidebar.tsx
@@ -1,13 +1,14 @@
 import CompanyDecorator from "@/src/models/CompanyDecorator";
 import SortDropDown from "./sorting/SortDropDown";
 import SortOptions from "@/src/util/sortOptions";
+import LocationGroup from "./filtering/LocationGroup";
 
 const Sidebar = (props: { updateSortState: (sortBy: SortOptions) => void }) => {
     const updateSortState = props.updateSortState;
 
     return (
         <div className="bg-base-100 h-full p-4 w-72">
-            <div className="">
+            <div>
                 <SortDropDown updateSortState={updateSortState} />
             </div>
             <div className="pt-4">
@@ -19,28 +20,8 @@ const Sidebar = (props: { updateSortState: (sortBy: SortOptions) => void }) => {
                 <p>Flexible days</p>
                 <input type="range" min={0} max="7" value="2" className="range range-xs" readOnly />
             </div>
-            <div className="form-control pt-4">
-                <h1 className="md:font-bold text-lg">Location</h1>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Aalborg</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Aarhus</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Copenhagen</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Herning</span>
-                </label>
-                <label className="label cursor-pointer justify-start space-x-2">
-                    <input type="checkbox" className="checkbox" />
-                    <span className="label-text">Odense</span>
-                </label>
+            <div>
+                <LocationGroup />
             </div>
         </div>
     );
--- src/app/overview/sidebar/filtering/LocationGroup.tsx
+++ src/app/overview/sidebar/filtering/LocationGroup.tsx
@@ -1,5 +1,29 @@
 const LocationGroup = () => {
-    return <div>LocationGroup</div>;
+    return (
+        <div className="form-control pt-4">
+            <h1 className="md:font-bold text-lg">Location</h1>
+            <label className="label cursor-pointer justify-start space-x-2">
+                <input type="checkbox" className="checkbox" />
+                <span className="label-text">Aalborg</span>
+            </label>
+            <label className="label cursor-pointer justify-start space-x-2">
+                <input type="checkbox" className="checkbox" />
+                <span className="label-text">Aarhus</span>
+            </label>
+            <label className="label cursor-pointer justify-start space-x-2">
+                <input type="checkbox" className="checkbox" />
+                <span className="label-text">Copenhagen</span>
+            </label>
+            <label className="label cursor-pointer justify-start space-x-2">
+                <input type="checkbox" className="checkbox" />
+                <span className="label-text">Herning</span>
+            </label>
+            <label className="label cursor-pointer justify-start space-x-2">
+                <input type="checkbox" className="checkbox" />
+                <span className="label-text">Odense</span>
+            </label>
+        </div>
+    );
 };
 
 export default LocationGroup;
