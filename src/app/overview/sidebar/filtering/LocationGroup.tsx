e98649546ddf89f440b5284659162a4b859c65c4
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-28 11:15:12 +0100

    Create array with the locations from the company list

--- src/app/overview/sidebar/filtering/LocationGroup.tsx
+++ src/app/overview/sidebar/filtering/LocationGroup.tsx
@@ -1,6 +1,22 @@
 import CompanyDecorator from "@/src/models/CompanyDecorator";
+import { useEffect, useState } from "react";
 
 const LocationGroup = (props: { allCompanies: CompanyDecorator[] }) => {
+    const _allCompanies: CompanyDecorator[] = props.allCompanies;
+    const [locations, setLocations] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (locations.length === 0) {
+            _allCompanies.forEach((company) => {
+                if (locations.includes(company.company.location)) {
+                    return;
+                }
+                locations.push(company.company.location);
+            });
+            setLocations([...locations.sort()]);
+        }
+    }, []);
+
     return (
         <div className="form-control pt-4">
             <h1 className="md:font-bold text-lg">Location</h1>
