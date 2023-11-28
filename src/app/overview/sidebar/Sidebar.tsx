ac782a6f752001aded0f028e86c5a554b4a448b2
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-28 12:26:47 +0100

    Move dummy data from page to SliderGroup

--- src/app/overview/sidebar/Sidebar.tsx
+++ src/app/overview/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import CompanyDecorator from "@/src/models/CompanyDecorator";
 import SortDropDown from "./sorting/SortDropDown";
 import SortOptions from "@/src/util/sortOptions";
 import LocationGroup from "./filtering/LocationGroup";
+import SliderGroup from "./filtering/SliderGroup";
 
 const Sidebar = (props: {
     allCompanies: CompanyDecorator[];
@@ -15,14 +16,8 @@ const Sidebar = (props: {
             <div>
                 <SortDropDown updateSortState={updateSortState} />
             </div>
-            <div className="pt-4">
-                <h1 className="md:font-bold text-lg">Filter</h1>
-                <p>Women in tech roles</p>
-                <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
-                <p>Women in leadership</p>
-                <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
-                <p>Flexible days</p>
-                <input type="range" min={0} max="7" value="2" className="range range-xs" readOnly />
+            <div>
+                <SliderGroup />
             </div>
             <div>
                 <LocationGroup allCompanies={props.allCompanies} updateLocationFilterState={props.updateLocationFilterState} />
--- src/app/overview/sidebar/filtering/SliderGroup.tsx
+++ src/app/overview/sidebar/filtering/SliderGroup.tsx
@@ -1,5 +1,15 @@
 const SliderGroup = () => {
-    return <div>SliderGroup</div>;
+    return (
+        <div className="pt-4">
+            <h1 className="md:font-bold text-lg">Filter</h1>
+            <p>Women in tech roles</p>
+            <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
+            <p>Women in leadership</p>
+            <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
+            <p>Flexible days</p>
+            <input type="range" min={0} max="7" value="2" className="range range-xs" readOnly />
+        </div>
+    );
 };
 
 export default SliderGroup;
