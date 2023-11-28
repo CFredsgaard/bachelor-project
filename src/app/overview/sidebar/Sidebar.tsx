b942b054956d1dde6f62dc7508a75b60d58ebdea
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-28 15:37:09 +0100

    Place DropDown over other elements when expanded

--- src/app/overview/sidebar/Sidebar.tsx
+++ src/app/overview/sidebar/Sidebar.tsx
@@ -13,14 +13,16 @@ const Sidebar = (props: {
 }) => {
     return (
         <div className="bg-base-100 h-full p-4 w-72">
-            <div>
+            <div className="absolute w-full pr-8 z-10">
                 <SortDropDown updateSortState={props.updateSortState} />
             </div>
-            <div>
-                <SliderGroup updateSlidersFilterState={props.updateSlidersFilterState} />
-            </div>
-            <div>
-                <LocationGroup allCompanies={props.allCompanies} updateLocationFilterState={props.updateLocationFilterState} />
+            <div className="pt-20">
+                <div>
+                    <SliderGroup updateSlidersFilterState={props.updateSlidersFilterState} />
+                </div>
+                <div>
+                    <LocationGroup allCompanies={props.allCompanies} updateLocationFilterState={props.updateLocationFilterState} />
+                </div>
             </div>
         </div>
     );
