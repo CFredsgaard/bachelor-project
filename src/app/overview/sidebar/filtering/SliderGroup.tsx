8569b556f6ffa2f76a2231351523e106085fe23c
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-11-28 13:55:20 +0100

    Use MUI Slider component in SliderGroup

--- src/app/overview/sidebar/filtering/SliderGroup.tsx
+++ src/app/overview/sidebar/filtering/SliderGroup.tsx
@@ -1,13 +1,58 @@
+import Slider from "@mui/material/Slider/Slider";
+import { useState } from "react";
+
 const SliderGroup = () => {
+    const [techRolesSlider, setTechRolesSlider] = useState<number[]>([0, 100]);
+
+    const handleSliderChange = (e: any, value: any) => {
+        console.log("Target: ", e.target.name);
+        console.log("The values", value);
+    };
     return (
         <div className="pt-4">
             <h1 className="md:font-bold text-lg">Filter</h1>
-            <p>Women in tech roles</p>
-            <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
-            <p>Women in leadership</p>
-            <input type="range" min={0} max="100" value="40" className="range range-xs" readOnly />
-            <p>Flexible days</p>
-            <input type="range" min={0} max="7" value="2" className="range range-xs" readOnly />
+
+            <div>
+                <p>Women in tech roles</p>
+                <div className="mx-2">
+                    <Slider
+                        getAriaLabel={() => "Women in tech"}
+                        getAriaValueText={() => "Testing slider"}
+                        defaultValue={[0, 100]}
+                        valueLabelDisplay="auto"
+                        color="info"
+                        onChangeCommitted={handleSliderChange}
+                        name={"aName"}
+                    />
+                </div>
+                <p>Women in leadership</p>
+                <div className="mx-2">
+                    <Slider
+                        getAriaLabel={() => "Temperature range"}
+                        defaultValue={[0, 100]}
+                        valueLabelDisplay="auto"
+                        getAriaValueText={() => "Some text"}
+                        color="info"
+                        onChangeCommitted={handleSliderChange}
+                        name="test1"
+                    />
+                </div>
+                <p>Flexible days</p>
+                <div className="mx-2">
+                    <Slider
+                        getAriaLabel={() => "Temperature range"}
+                        defaultValue={[0, 7]}
+                        valueLabelDisplay="auto"
+                        getAriaValueText={() => "Some text"}
+                        step={1}
+                        marks
+                        min={0}
+                        max={7}
+                        color="info"
+                        onChangeCommitted={handleSliderChange}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
