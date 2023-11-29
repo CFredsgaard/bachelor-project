cbb692829055ac86450c1ac5f98b79069493fb59
Kaan <kaan1705@gmail.com>
2023-11-29 17:50:09 +0100

    Move components to correct directory

--- src/app/overview/header/Header.tsx
+++ src/app/overview/header/Header.tsx
@@ -1,21 +1,32 @@
 import Link from "next/link";
 import Searchbar from "./Searchbar";
+import CompanyDecorator from "@/app/models/CompanyDecorator";
 
-const Header = (props: { updateSearchState: (name: string, location: string) => void }) => {
-    return (
-        <div className="navbar bg-base-100 grid grid-cols-5">
-            <div className="h-full">
-                <Link href="/" className="h-full">
-                    <p className="text-3xl font-semibold text-slate-700 h-full flex flex-wrap p-4 place-content-evenly">
-                        Work Life Balance
-                    </p>
-                </Link>
-            </div>
-            <div className="self-center col-start-3 col-span-1">
-                <Searchbar updateSearchState={props.updateSearchState} />
-            </div>
-        </div>
-    );
+const Header = (props: {
+  allCompanies: CompanyDecorator[];
+  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+  updateShouldSort: (shouldSort: boolean) => void;
+}) => {
+  return (
+    <div className="navbar bg-base-100 grid grid-cols-5">
+      <div className="h-full">
+        <Link href="/" className="h-full">
+          <img
+            src="/icons/logo overview.svg"
+            alt="Logo"
+            className="w-40 h-14"
+          />
+        </Link>
+      </div>
+      <div className="self-center col-span-1">
+        <Searchbar
+          updateCompanies={props.updateCompanies}
+          allCompanies={props.allCompanies}
+          updateShouldSort={props.updateShouldSort}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/app/overview/header/Searchbar.tsx
+++ src/app/overview/header/Searchbar.tsx
@@ -1,50 +1,79 @@
-import { useState } from "react";
+import CompanyDecorator from "@/app/models/CompanyDecorator";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 
-const Searchbar = (props: { updateSearchState: (name: string, location: string) => void }) => {
-    const [nameInput, setNameInput] = useState<string>("");
-    const [locationInput, setLocationInput] = useState<string>("");
+interface CompanySearchBarProps {
+  allCompanies: CompanyDecorator[];
+  updateCompanies: (selectedCompanies: CompanyDecorator[]) => void;
+  updateShouldSort: (shouldSort: boolean) => void;
+}
 
-    const handleSearch = () => {
-        props.updateSearchState(nameInput, locationInput);
-    };
+const Searchbar: React.FC<CompanySearchBarProps> = ({
+  allCompanies,
+  updateCompanies,
+  updateShouldSort,
+}) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [locationTerm, setLocationTerm] = useState<string>("");
+  const searchParams = useSearchParams();
 
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter") {
-            handleSearch();
-        }
-    };
+  useEffect(() => {
+    if (searchParams.get("searchTerm1") || searchParams.get("searchTerm2")) {
+      setSearchTerm(searchParams.get("searchTerm1") ?? "");
+      setLocationTerm(searchParams.get("searchTerm2") ?? "");
 
-    const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Backspace") {
-            handleSearch();
-        }
-    };
+      handleSearch();
+    }
+  });
 
-    return (
-        <div className="grid gap-4 grid-flow-col">
-            {/* company Search Bar */}
-            <input
-                type="text"
-                placeholder="Company name"
-                className="input input-bordered input-primary input-md"
-                onChange={(e) => setNameInput(e.target.value)}
-                onKeyDown={handleKeyDown}
-                onKeyUp={handleKeyUp}
-            />
-            {/* Location Search Bar */}
-            <input
-                type="text"
-                placeholder="Location"
-                className="input input-bordered input-primary input-md"
-                onChange={(e) => setLocationInput(e.target.value)}
-                onKeyDown={handleKeyDown}
-                onKeyUp={handleKeyUp}
-            />
-            <button className="btn btn-primary btn-md text-white" onClick={handleSearch}>
-                Search
-            </button>
-        </div>
+  const handleSearch = () => {
+    //Retrieve all companies
+    const companyList = [...allCompanies];
+
+    //Filter based on search terms
+    const filteredData = companyList.filter(
+      (item) =>
+        item.company.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        item.company.location.toLowerCase().includes(locationTerm.toLowerCase())
     );
+
+    //Display companies matching search terms
+    updateCompanies(filteredData);
+    updateShouldSort(true);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="grid gap-4 grid-flow-col">
+      {/* company Search Bar */}
+      <input
+        type="text"
+        placeholder="Search for a company..."
+        className="input input-bordered input-primary input-md"
+        onChange={(evt) => setSearchTerm(evt.target.value)}
+        onKeyDown={handleKeyPress} // Listen for Enter key press
+      />
+      {/* Location Search Bar */}
+      <input
+        type="text"
+        placeholder="Search by location..."
+        className="input input-bordered input-primary input-md"
+        onChange={(evt) => setLocationTerm(evt.target.value)}
+        onKeyDown={handleKeyPress} // Listen for Enter key press
+      />
+      <button
+        className="btn btn-outline btn-primary btn-md"
+        onClick={handleSearch}
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default Searchbar;
