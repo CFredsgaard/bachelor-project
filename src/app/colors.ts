9f511461a2a2f80b7af10dbb04e373a5f6e3cff2
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-12-02 17:44:31 +0100

    Add Colors file

---
+++ src/app/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+    navy: "#265073",
+    darkteal: "#138D75",
+    lightteal: "#9AD0C2",
+    yellowgrey: "#ECF4D6",
+    color1: "#053B50",
+    color2: "#176B87",
+    color3: "#64CCC5",
+    color4: "#EEEEEE",
+};
