c7c854a7eeb0f55f64856c318fde76c15823cb11
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-12-02 17:26:03 +0100

    Move searchbar back to center of header

--- src/app/overview/header/Header.tsx
+++ src/app/overview/header/Header.tsx
@@ -2,22 +2,18 @@ import Link from "next/link";
 import Searchbar from "./Searchbar";
 
 const Header = (props: { updateSearchState: (name: string, location: string) => void }) => {
-  return (
-    <div className="navbar bg-base-100 grid grid-cols-5">
-      <div className="h-full">
-        <Link href="/" className="h-full">
-          <img
-            src="/icons/logohorizontal.svg"
-            alt="Logo"
-            className="h-full"
-          />
-        </Link>
-      </div>
-      <div className="self-center col-span-1">
-        <Searchbar updateSearchState={props.updateSearchState} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar bg-base-100 grid grid-cols-5">
+            <div className="h-full col-span-2">
+                <Link href="/" className="h-full">
+                    <img src="/icons/logohorizontal.svg" alt="Logo" className="h-full" />
+                </Link>
+            </div>
+            <div className="self-center col-span-3">
+                <Searchbar updateSearchState={props.updateSearchState} />
+            </div>
+        </div>
+    );
 };
 
 export default Header;
