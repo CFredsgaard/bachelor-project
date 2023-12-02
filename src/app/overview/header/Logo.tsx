791f1f5b37fa658c263f2481972f3d7756736349
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-12-02 22:40:34 +0100

    Logo in header aling with content in sidebar

--- src/app/overview/header/Logo.tsx
+++ src/app/overview/header/Logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Logo = () => {
     return (
         <Link href="/" className="flex items-center">
-            <img src="/icons/logo.svg" alt="Logo" className="w-1/6" />
+            <img src="/icons/logo.svg" alt="Logo" className="w-1/6 ml-2" />
 
             <img src="/icons/name_dark.svg" alt="Name" className=" w-1/2" color="#EEEEEE" />
         </Link>
