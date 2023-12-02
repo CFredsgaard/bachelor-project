c6d9c3c673a01a885aa177574d37fac0b685ef16
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-12-02 17:27:33 +0100

    Give padding to header again

--- src/app/overview/header/Header.tsx
+++ src/app/overview/header/Header.tsx
@@ -3,7 +3,7 @@ import Searchbar from "./Searchbar";
 
 const Header = (props: { updateSearchState: (name: string, location: string) => void }) => {
     return (
-        <div className="navbar bg-base-100 grid grid-cols-5">
+        <div className="navbar bg-base-100 grid grid-cols-5 p-4">
             <div className="h-full col-span-2">
                 <Link href="/" className="h-full">
                     <img src="/icons/logohorizontal.svg" alt="Logo" className="h-full" />
